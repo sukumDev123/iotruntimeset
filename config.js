cfa697997244acc5c8ad561d4c4637bd30c0ada5
sukumDev123 <kungo2009@hotmail.com>
2018-11-28 23:29:10 +0700

    set time to open and this project use firebase realtime data base. :fire: :fire:

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  apiKey: "AIzaSyCyNEMG2Is18OPHku1x-a50LuykNdw_P3A",
-  authDomain: "my-project-s-handler-bulbs.firebaseapp.com",
-  databaseURL: "https://my-project-s-handler-bulbs.firebaseio.com",
-  projectId: "my-project-s-handler-bulbs",
-  storageBucket: "my-project-s-handler-bulbs.appspot.com",
-  messagingSenderId: "1095133076333"
-};
